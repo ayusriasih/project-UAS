5497c5efcd6d9671aab515910eb671458d2e9ec0
ayusriasih <ketutayu724@gmail.com>
2026-01-23 23:28:27 +0800

    Update PenitipanDetailScreen.tsx

--- kode/PenitipanDetailScreen.tsx
+++ kode/PenitipanDetailScreen.tsx
@@ -29,7 +29,6 @@ const PenitipanDetailScreen = () => {
   const [harga, setHarga] = useState("");
   const [status, setStatus] = useState("Pending");
 
-  // 🔥 AUTO ISI DARI KERANJANG
   useEffect(() => {
     if (route.params) {
       setLayanan(route.params.layanan || "");
